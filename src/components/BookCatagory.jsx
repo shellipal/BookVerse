869441d83c7b582aca9a3e5078bcfc617d3b628c
shellipal <palshelli5@gmail.com>
2025-10-25 22:25:38 +0530

    fix typo

--- src/components/BookCatagory.jsx
+++ src/components/BookCatagory.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { googleApiKey, googleBaseUrl } from "../utils";
-import { Heart, Search } from "lucide-react";
 import BookCard from "./BookCard";
 
 const catagoryButtons = [
